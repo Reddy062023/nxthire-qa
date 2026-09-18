128cd022141a402bdcb96154a20cb84542cb077f
Japendra <japendrareddy@gmail.com>
2026-09-17 20:46:33 -0400

    Exclude archived legacy suite from test runs

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  testIgnore: ['**/archive/**'],
   timeout: 60000,
   retries: 1,
   reporter: [['list'], ['allure-playwright']],
